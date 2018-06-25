83915e4b4542abb4b3e618fffe3da91e845dcd02
Jan Ole Suhr <ole.suhr@rocket-es.com>
2018-06-25 11:14:10 +0200

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Jan Ole Suhr on 25.06.18.
+//  Copyright © 2018 Jan Ole Suhr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
